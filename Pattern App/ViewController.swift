c54bef38a8e7aee066b10825ef6d00ccce8cd21c
Your Name <you@example.com>
2018-11-15 14:12:16 -0700

    Initial Commit

---
+++ Pattern App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pattern App
+//
+//  Created by Anton Miguel de Guia on 2018-11-15.
+//  Copyright © 2018 Phidgets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
